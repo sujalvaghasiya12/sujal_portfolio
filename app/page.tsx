e4256ff05491cc8cf7da5e5c263cc96b62beeae1
sujalvaghasiya12 <22amtics067@gmail.com>
2025-11-29 06:28:14 -0500

    Initial portfolio upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Navbar from "@/components/navbar"
 import Hero from "@/components/sections/hero"
 import Skills from "@/components/sections/skills"
@@ -10,9 +10,25 @@ import About from "@/components/sections/about"
 import Certificates from "@/components/sections/certificates"
 import Contact from "@/components/sections/contact"
 import Footer from "@/components/footer"
+import LoadingScreen from "@/components/sections/LoadingScreen"
 
 export default function Home() {
   const [activeSection, setActiveSection] = useState("home")
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    // Simulate loading time
+    const timer = setTimeout(() => {
+      setIsLoading(false)
+    }, 2000) // Show loading screen for 2 seconds
+
+    return () => clearTimeout(timer)
+  }, [])
+
+  // Show loading screen while isLoading is true
+  if (isLoading) {
+    return <LoadingScreen />
+  }
 
   return (
     <div className="w-full overflow-hidden bg-white">
